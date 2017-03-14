dd51269f09899c8d75a7fb3aa2b301bf72f1c233
alvyang <626183528@qq.com>
2017-03-14 22:01:49 +0800

    swiper

--- views/home.vue
+++ views/home.vue
@@ -1,14 +1,30 @@
 <template>
 	<div>
 		<jf-top></jf-top>
-		<div>123</div>
+		<div class="swiper-container">
+		  <div class="swiper-wrapper">
+		    	<div class="swiper-slide">
+		    		<img src="../img/swiper01.jpg" />
+		    	</div>
+		    <div class="swiper-slide">
+		    		<img src="../img/swiper02.jpg" />
+		    </div>
+		  </div>
+		</div>
 	</div>
 </template>
 <script>
 	import Top from "components/top.vue";
+	import swiper from "swiper";
+	require("swiper/dist/css/swiper.min.css");
 	export default({
 		data(){
 			return {}
+		},
+		mounted(){
+			var mySwiper = new Swiper('.swiper-container', {
+				autoplay: 2000,//可选选项，自动滑动
+			})
 		},
 		components:{
 			"jf-top":Top
@@ -16,5 +32,10 @@
 	})
 </script>
 <style>
-	
+	.swiper-container{
+		margin-top: -1px;
+	}
+	.swiper-slide img{
+		width: 10rem;
+	}
 </style>
